e38a2e2a9f801143a5fc5a19638376ee018c7b6d
Hannah Joy Graves <hannahjoygraves@Hannahs-MacBook-Air.local>
2019-10-05 14:56:54 +0200

    Changed the HTML for the site.

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g-w-f^5!37pjbo$hw%@e!%()=)t6=8^!3(yay9xud_raml*+mi'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
